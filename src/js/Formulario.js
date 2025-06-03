e9cc799ef8296ef8a7a9d991b63d6e5b077504e5
Henrique0602 <henriquef.brigo@gmail.com>
2025-06-02 21:10:07 -0300

    Criação do Formulario

---
+++ src/js/Formulario.js
@@ -0,0 +1,13 @@
+document.getElementById('contatoForm').addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const email = document.getElementById("email").value.trim()
+    const mensagem  = document.getElementById("mensagem").value.trim()
+
+    if(!email || !mensagem) {
+        alert('Por favor, preencha todos os campos ')
+        return
+    }
+
+
+    alert('Mensagem enviada com sucesso!');
+})
--- src/js/mudancaTema.js
+++ src/js/mudancaTema.js
@@ -2,38 +2,35 @@ const BotaoDark = document.getElementById("botao1");
 const BotaoLight = document.getElementById("botao2");
 const BotaoNormal = document.getElementById("botao3");
 
-BotaoDark.addEventListener("click", () => {
+BotaoDark.addEventListener("click", (e) => {
+  e.preventDefault
   document.querySelectorAll("header, section, footer,nav").forEach((el) => {
     el.style.background = "#121212";
   });
 
-  document
-    .querySelectorAll("section h2, section p, a , li, img")
-    .forEach((el) => {
+  document.querySelectorAll("section h2, section p, a , li, img").forEach((el) => {
       el.style.color = "#ffffff";
     });
 });
 
-BotaoLight.addEventListener("click", () => {
+BotaoLight.addEventListener("click", (e) => {
+  e.preventDefault
   document.querySelectorAll("header, section, footer, nav").forEach((el) => {
     el.style.background = "#ffffff";
   });
 
-  document
-    .querySelectorAll("section h2, section p, a , li, img")
-    .forEach((el) => {
+  document.querySelectorAll("section h2, section p, a , li, img").forEach((el) => {
       el.style.color = "#121212";
     });
 });
 
-BotaoNormal.addEventListener("click", () => {
+BotaoNormal.addEventListener("click", (e) => {
+  e.preventDefault
   document.querySelectorAll("header, section, footer, nav").forEach((el) => {
     el.style.background = "";
   });
 
-  document
-    .querySelectorAll("section h2, section p, a , li, img")
-    .forEach((el) => {
+  document.querySelectorAll("section h2, section p, a , li, img").forEach((el) => {
       el.style.color = "";
     });
 });
